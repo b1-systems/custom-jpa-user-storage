841ecfbd627dab251527ba01af1ca4c1c780b004
Tilman Kranz <tilman.kranz@b1-systems.de>
2026-06-05 05:35:36 +0200

    correct copyright information

--- src/main/java/de/b1systems/keycloak/storage/user/RealmRoleEntity.java
+++ src/main/java/de/b1systems/keycloak/storage/user/RealmRoleEntity.java
@@ -1,4 +1,4 @@
-/* Copyright 2024  B1 Systems GmbH <info@b1-systems.de>
+/* Copyright 2024-2026  B1 Systems GmbH <info@b1-systems.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
